9eb40341df23524f95ccd532e787218f45142c1f
Arys <poluektovdmytro@gmail.com>
2026-06-05 00:05:23 -0700

    added mnga carousels

--- app/(roots)/anime/page.tsx
+++ app/(roots)/anime/page.tsx
@@ -1,41 +1,11 @@
-import CarouselSection from "@/components/carouselSection";
-import {fetchTrendingMedia,fetchUpcomingAnime,fetchAllTimePopularMedia,} from "@/lib/anilist";
-import { getLikedAnimeIds } from "@/components/getLikedAnimeIds";
-import { getSavedAnimeIds } from "@/components/getSavedAmimeIds";
+
 
 export default async function Home() {
   
-  const [trending,upcoming,popular, likedAnimeIds, savedAnimeIds] = await Promise.all([
-    fetchTrendingMedia(1, "ANIME"),
-    fetchUpcomingAnime(1),
-    fetchAllTimePopularMedia(1, "ANIME"),
-    getLikedAnimeIds(),
-    getSavedAnimeIds(),
-  ]);
 
   return (
-    <main className="flex flex-col justify-center items-center py-2 md:py-6 gap-2 min-[1001px]:gap-6">
-      <CarouselSection
-        animeList={trending}
-        title="TRENDING"
-        likedAnimeIds={likedAnimeIds}
-        savedAnimeIds={savedAnimeIds}
-
-      />
-      <CarouselSection
-        animeList={upcoming}
-        title="UPCOMING"
-        likedAnimeIds={likedAnimeIds}
-        savedAnimeIds={savedAnimeIds}
-      />
-
-      <CarouselSection
-        animeList={popular}
-        title="ALL TIME POPULAR"
-        likedAnimeIds={likedAnimeIds}
-        savedAnimeIds={savedAnimeIds}
-
-      />
-    </main>
+   <div>
+    
+   </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,62 @@
+import CarouselSection from "@/components/carouselSection";
+import {fetchTrendingMedia,fetchUpcomingAnime,fetchAllTimePopularMedia,} from "@/lib/anilist";
+import { getLikedAnimeIds } from "@/components/getLikedAnimeIds";
+import { getSavedAnimeIds } from "@/components/getSavedAmimeIds";
 
-const page = () => {
+export default async function Home() {
+  
+  const [trendingAnime,upcoming,popularAnime, likedAnimeIds, savedAnimeIds, trendingManga, popularManga] = await Promise.all([
+    fetchTrendingMedia(1, "ANIME"),
+    fetchUpcomingAnime(1),
+    fetchAllTimePopularMedia(1, "ANIME"),
+    getLikedAnimeIds(),
+    getSavedAnimeIds(),
+    fetchTrendingMedia(1, "MANGA"),
+    fetchAllTimePopularMedia(1, "MANGA"),
+
+
+  ]);
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <main className="flex flex-col justify-center items-center py-2 md:py-6 gap-2 min-[1001px]:gap-6">
+      <CarouselSection
+        animeList={trendingAnime}
+        title="TRENDING ANIME"
+        likedAnimeIds={likedAnimeIds}
+        savedAnimeIds={savedAnimeIds}
 
-export default page
+      />
+      <CarouselSection
+        animeList={upcoming}
+        title="UPCOMING ANIME"
+        likedAnimeIds={likedAnimeIds}
+        savedAnimeIds={savedAnimeIds}
+      />
+
+      <CarouselSection
+        animeList={popularAnime}
+        title="ALL TIME POPULAR ANIME"
+        likedAnimeIds={likedAnimeIds}
+        savedAnimeIds={savedAnimeIds}
+
+      />
+          <CarouselSection
+        animeList={trendingManga}
+        title="TRENDING MANGA"
+        likedAnimeIds={likedAnimeIds}
+        savedAnimeIds={savedAnimeIds}
+
+      />
+
+            <CarouselSection
+        animeList={popularManga}
+        title="ALL TIME POPULAR MANGA"
+        likedAnimeIds={likedAnimeIds}
+        savedAnimeIds={savedAnimeIds}
+
+      />
+      
+      
+    </main>
+  );
+}
